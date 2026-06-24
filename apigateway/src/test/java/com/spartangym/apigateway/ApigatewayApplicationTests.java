210e1bf582fb1ef3d93d8972e51fa49c6bb165bb
Francisco Osorio <fran.osoriov@duocuc.cl>
2026-06-24 16:46:15 -0400

    agregar API Gateway con Eureka y rutas de microservicios

---
+++ apigateway/src/test/java/com/spartangym/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartangym.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
