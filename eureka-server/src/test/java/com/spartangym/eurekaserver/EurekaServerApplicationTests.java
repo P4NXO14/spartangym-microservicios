45cc42ef312c18612b8b724402a501eb0c69dbc3
Francisco Osorio <fran.osoriov@duocuc.cl>
2026-06-24 13:46:32 -0400

    agregar servidor eureka

---
+++ eureka-server/src/test/java/com/spartangym/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartangym.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
